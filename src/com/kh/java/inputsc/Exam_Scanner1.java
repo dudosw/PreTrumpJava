2e5f3102570979cf1b0aa929cfecfd402d4eb1c7
user1 <user1@DESKTOP-QBPCST0>
2024-11-15 12:42:12 +0900

    [ADD] 24/11/15 입력스캐너 정리
    1. 스캐너에 대해서 정리함
    2. 다양한 명령어를 통해 입력받을수 있음

---
+++ src/com/kh/java/inputsc/Exam_Scanner1.java
@@ -0,0 +1,19 @@
+package com.kh.java.inputsc;
+
+public class Exam_Scanner1 {
+	public static void main(String [] args) {
+		/*
+		 * Scanner : Java API, 이미 만들어진 클래스
+		 * 키보드를 통해 들어온 입력값을 사용할 수 있도록 해줌
+		 * 사용법 : Scanner변수명 = new Scanner(System.in);
+		 * 변수명 외에는 바뀔 수 없음, 그대로 적어야 함.
+		 * Scanner를 썼을 때 반드시 import(ctrl+space bar)를 
+		 * 해주어야함
+		 *
+		 */
+		Scanner sc = new Scanner(System.in);
+		// 실제 입력받기 위해서는
+		// sc.nextInt()를 적어야함
+		//next뒤에 int를 쓰면 int를, doble을 쓰면 
+	}
+}
